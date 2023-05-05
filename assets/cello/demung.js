3a3e921d300041f49118dda11c08517832a749a0
rihandcreative <87368106+elfradodee@users.noreply.github.com>
2023-05-05 23:40:35 +0700

    Karawitan: Initial Commit

---
+++ assets/cello/demung.js
@@ -0,0 +1,9 @@
+function playSlendro(notasi) {
+    var music = new Audio('https://kawruhbasa.com/wp-content/uploads/2023/02/Suara-nada-' + notasi + '-demung-slendro.wav');
+    music.play();
+}
+
+function playPelog(notasi) {
+    var music = new Audio('https://kawruhbasa.com/wp-content/uploads/2023/02/Suara-nada-' + notasi + '-demung-pelog.wav');
+    music.play();
+}
